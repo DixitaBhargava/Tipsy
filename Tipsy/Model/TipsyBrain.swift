27c26b58ce2e935dc21004ab762af8b0585fa1ab
Dixita Bhargava <dixitabhargava@MacBook-Pro.local>
2020-06-22 22:02:20 +0530

    Tipsy Updated

---
+++ Tipsy/Model/TipsyBrain.swift
@@ -0,0 +1,12 @@
+//
+//  TipsyBrain.swift
+//  Tipsy
+//
+//  Created by Dixita Bhargava on 11/06/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct TipsyBrain {
+    var tip : Int
+}
